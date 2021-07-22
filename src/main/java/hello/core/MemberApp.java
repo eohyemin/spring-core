30ba73856f83fc4dfb5c86de4a048084d83369a0
eohyemin <eohyemin@gmail.com>
2021-07-22 14:50:36 +0900

    add memberService Test

---
+++ src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,17 @@
+package hello.core;
+
+import hello.core.member.domain.Grade;
+import hello.core.member.domain.Member;
+import hello.core.member.service.MemberService;
+import hello.core.member.service.MemberServiceImpl;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println(findMember.getName());
+    }
+}
